dc12fea17519bf262b0f5a870ad17aa56bfd7261
NordItaly25 <197090171+Norditaly25@users.noreply.github.com>
2025-02-18 20:45:36 +0100

    Finished step 2 of this project by transforming some pseudocode i wrote into javascript using a function with some if...else if statements

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
 console.log("Hello World!");
+
+// Create a function called getComputerChoice
+// Generate a random number between 0 and 1
+// If the number is < 1/3, return "rock"
+// If the number is between 1/3 and 2/3, return "paper"
+// If the number is >= 2/3, return "scissors"
+// Log the function call to check the output
+
+function getComputerChoice() {
+    let randomNum = Math.random ();
+        if (randomNum <= 1/3 ) {
+            return "rock";
+        }
+        else if (randomNum <=2/3) {
+            return "paper";
+        }
+        else {
+            return "scissors";
+        }
+    
+    }
+console.log(getComputerChoice());
